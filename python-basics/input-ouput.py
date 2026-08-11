b7e9a1d5e3acf2658020dd54bf83cc3cb323ecbf
Aakriti Singh <aakritisingh496@gmail.com>
2026-08-11 09:18:10 +0530

    Add Python input and output examples

---
+++ python-basics/input-ouput.py
@@ -0,0 +1,16 @@
+# one can take data from user using input() statement
+# one can display data to user through print() statement
+
+# Python Input-Output
+
+
+# Taking Input
+name = input("what's your name? \n")
+age = input("What's your Age? \n")
+zodiac_sign = input("What's your zodiac/moon sign?")
+
+# Displaying Output
+print("\nPERSON DETAILS:")
+print("Name:", name)
+print("Age:" , age)
+print("Zodiac Sign:", zodiac_sign)
