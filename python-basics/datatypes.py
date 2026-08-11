5af91b39c5b69b317c6b5989f7cb3171814ff3b0
Aakriti Singh <aakritisingh496@gmail.com>
2026-08-11 09:15:46 +0530

    Add Python data types examples

---
+++ python-basics/datatypes.py
@@ -0,0 +1,54 @@
+# datatypes specifies the type of values a variable holds
+
+# Python Datatypes
+
+# 1. Integer
+age = 20
+print("Integer:", age)
+print(type(age))
+
+
+# 2. Float
+height = 5.6
+print("\nFloat:", height)
+print(type(height))
+
+
+# 3. String
+name = "XYZ"
+print("\nString:", name)
+print(type(name))
+
+
+# 4. Boolean
+is_student = True
+print("\nBoolean:", is_student)
+print(type(is_student))
+
+
+# 5. List
+fruits = ["Apple", "Banana", "Mango"]
+print("\nList:", fruits)
+print(type(fruits))
+
+
+# 6. Tuple
+student = ("xyz",19,"Computer Engineering")
+print("\nTuple:", student)
+print(type(student))
+
+
+# 7. Set
+numbers = {1, 2, 3, 4}
+print("\nSet:", numbers)
+print(type(numbers))
+
+
+# 8. Dictionary
+student = {
+    "name": "XYZ",
+    "age": 19,
+    "course": "Computer Engineering"
+}
+print("\nDictionary:", student)
+print(type(student))
